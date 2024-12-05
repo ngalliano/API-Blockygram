2a3e8bda894f605c46f7428cbc9ee1baabd6ce02
ngalliano <nahuel.galliano@gmail.com>
2024-12-05 14:53:30 -0300

    corregido error en update EstadisticasNivelesDiarios, asi como se cambio para que el topJugador muestre nombreUsuario en vez de idUsuario

--- controllers/estadisticasNivelDiario.js
+++ controllers/estadisticasNivelDiario.js
@@ -94,17 +94,30 @@ class dailyLevelStatController{
             console.log("hola");
             //console.log(nivel);
             const lista1 = await dailyLevelStatsModel.findAll({
-                attributes: ['puestoClasificacion','idUsuario','tiempoResolucion'],
+                attributes: ['puestoClasificacion','idUsuario','nombreUsuario','tiempoResolucion'],
                 where: {idNivel:parseInt(fecha2)},
                 order: [['tiempoResolucion', 'ASC']]
             });
             
             const lista2 = lista1.map((user, index) => ({
                 idUsuario: user.idUsuario,
+                nombreUsuario: user.nombreUsuario,
                 puestoClasificacion: index + 1,
                 tiempoResolucion: user.tiempoResolucion
             }));
-                        
+            
+            class Registro{
+                constructor (nombreUsuario, puestoClasificacion, tiempoResolucion){
+                    this.nombreUsuario = nombreUsuario;
+                    this.puestoClasificacion = puestoClasificacion;
+                    this.tiempoResolucion = tiempoResolucion;
+                }
+            }
+            
+            const lista3 = [];
+            lista2.forEach(element => {
+                lista3.push(new Registro (element.nombreUsuario, element.puestoClasificacion, element.tiempoResolucion));
+            });
             //console.log(lista2[0].idUsuario);
             const player = lista2.find((element) => element.idUsuario === idUsuario1)
             if (player == undefined){
@@ -115,44 +128,44 @@ class dailyLevelStatController{
             console.log("hola");
             
             const leaderBoard = [];
-                        
-            console.log(lista2.length);
+            console.log(lista2.length);            
+            console.log(lista3.length);
             console.log(player.puestoClasificacion);
-            if (lista2.length > 20){
+            if (lista3.length > 20){
                 if (player.puestoClasificacion < 11){
                     for (let i=0; i < player.puestoClasificacion; i++){
-                        leaderBoard[i] = lista2[i];
+                        leaderBoard[i] = lista3[i];
                     }
                     for (let i=player.puestoClasificacion; i<20; i++){
-                        leaderBoard[i] = lista2[i];
+                        leaderBoard[i] = lista3[i];
                     }
                 }
-                if (player.puestoClasificacion >= 11 && player.puestoClasificacion < lista2.length-9){
+                if (player.puestoClasificacion >= 11 && player.puestoClasificacion < lista3.length-9){
                     let j=0;
                     for (let i=player.puestoClasificacion-10; i<player.puestoClasificacion; i++){
-                        leaderBoard[j] = lista2[i];
+                        leaderBoard[j] = lista3[i];
                         j++;
                         console.log(j);
                         console.log(i);
                     }
                     console.log("AAA");
                     for (let i=player.puestoClasificacion; i<player.puestoClasificacion+10; i++){
-                        leaderBoard[j] = lista2[i];
+                        leaderBoard[j] = lista3[i];
                         j++;
                         console.log(j);
                         console.log(i);
                     }
                 }
-                if (player.puestoClasificacion >= lista2.length-9){
+                if (player.puestoClasificacion >= lista3.length-9){
                     
                     for (let i=0; i<20; i++){
-                        leaderBoard[19-i] = lista2[lista2.length-1-i];
+                        leaderBoard[19-i] = lista3[lista3.length-1-i];
                     }
                 }
             }
             else{
-                for (let i = 0; i<lista2.length; i++){
-                    leaderBoard[i] = lista2[i];
+                for (let i = 0; i<lista3.length; i++){
+                    leaderBoard[i] = lista3[i];
                 }
             }
                         
@@ -196,7 +209,7 @@ class dailyLevelStatController{
                     {message: 'Data in body and query of request is different'}
                 );
             }
-            const nivel = await dailyLevelStatsModel.update({puestoClasificacion:data1.puestoClasificacion, estadoClasificacion1:data1.estadoClasificacion1},
+            const nivel = await dailyLevelStatsModel.update({nombreUsuario:data1.nombreUsuario,puestoClasificacion:data1.puestoClasificacion, estadoClasificacion1:data1.estadoClasificacion1},
                 {where: {idNivel:idNivel1, idUsuario:idUsuario1}});
             //console.log('Hola');            
             if (typeof (nivel[0]) != 'undefined' && nivel[0] === 1){
