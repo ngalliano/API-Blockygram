8c719f29a65e37abc029d4cbd91a28f1ab60e79a
Augusto Davila <augustodavila.lg@gmail.com>
2025-01-18 13:41:53 -0300

    agregada configuración de ESLint y reglas personalizadas para mejorar la calidad del código

---
+++ eslint-custom-rules/index.js
@@ -0,0 +1,95 @@
+  const plugin = {
+
+    // preferred location of name and version
+    meta: {
+        name: "eslint-custom-rules",
+        version: "1"
+    },
+    rules: {
+        "max-nested-for": {
+          create(context) {
+            let loopDepth = 0;
+    
+            return {
+              ForStatement(node) {
+                loopDepth++;
+                if (loopDepth > 2) {
+                  context.report({
+                    node,
+                    message: "No puedes tener 3 o mas bucles 'for' anidados."
+                  });
+                }
+              },
+              "ForStatement:exit"() {
+                loopDepth--;
+              }
+            };
+          }
+        },
+        "variableNameLength": {
+          meta: {
+            type: "suggestion",
+            docs: {
+              description: "Limita la longitud de los nombres de las variables a un máximo de 15 caracteres",
+              recommended: false, 
+            },
+            schema: [
+              {
+                type: "object",
+                properties: {
+                  maxLength: {
+                    type: "number",
+                    default: 20, 
+                  },
+                  minLength: {
+                    type: "number",
+                    default: 3, 
+                  }
+                },
+                additionalProperties: false,
+              },
+            ],
+            messages: {
+              tooLong: "El nombre de la variable '{{ name }}' tiene más de {{ maxLength }} caracteres. Actualmente tiene {{ length }} caracteres.",
+              tooShort: "El nombre de la variable '{{ name }}' tiene menos de {{ minLength }} caracteres. Actualmente tiene {{ length }} caracteres.",
+            },
+          },
+          create(context) {
+            const maxLength = context.options[0]?.maxLength; 
+            const minLength = context.options[0]?.minLength
+            return {
+              VariableDeclarator(node) {
+                const variableName = node.id.name;
+                if (variableName && variableName.length > maxLength) {
+                  context.report({
+                    node: node.id,
+                    messageId: "tooLong",
+                    data: {
+                      name: variableName,
+                      maxLength,
+                      length: variableName.length,
+                    },
+                  });
+                }else{
+                  if (variableName && (variableName != "i") && (variableName != "j") && (variableName != "k") && variableName.length < minLength){
+                    context.report({
+                      node: node.id,
+                      messageId: "tooShort",
+                      data: {
+                        name: variableName,
+                        minLength,
+                        length: variableName.length,
+                      },
+                    });
+                  }
+                }
+              },
+            };
+          },
+        }
+        
+      }
+};
+
+// for ESM
+export default plugin;
---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import globals from "globals";
+import path from 'path';
+import eslintCustomRules from './eslint-custom-rules/index.js';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    plugins: {
+      "eslint-custom-rules": eslintCustomRules
+    },
+    rules: {
+      "max-lines": ["warn", { "max": 300, "skipBlankLines": true, "skipComments": true }],
+      "camelcase": ["error", { "properties": "always" }],
+      "prefer-const": "error",
+      "eslint-custom-rules/max-nested-for": "error",
+      "eslint-custom-rules/variableNameLength": ["error", { "maxLength": 20, "minLength": 3 }],
+    }
+  }
+];
